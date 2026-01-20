4bed6b283d20835d9c178205b15aa04605526118
josgralagu <josgralagu@hotmail.com>
2026-01-20 16:17:49 -0600

    Second experimental fix

--- src/configs/utils/auth-helper.ts
+++ src/configs/utils/auth-helper.ts
@@ -20,7 +20,7 @@ export async function authenticateNewUser (page: Page) {
     console.log(`📍 [1/4] Navigating to sign up page...`)
     await signUpPage.navigateToSignUp()
     //await page.waitForLoadState('domcontentloaded')
-    await signUpPage.firstNameField.waitFor({ state: 'visible', timeout: 15000 })
+    await signUpPage.firstNameField.waitFor({ state: 'attached', timeout: 15000 })
     console.log(`✅ [1/4] Navigated to sign up`)
 
     console.log(`📍 [2/4] Completing registration...`)
